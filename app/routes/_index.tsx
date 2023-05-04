74a313759859d5a0a15bbe8d2f7193441a3da883
Jack Howard <jack.howard@expel.io>
2023-05-04 14:08:56 -0700

    feat: Update entry page

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -8,77 +8,54 @@ export const meta: V2_MetaFunction = () => [{ title: "Kind Retro App" }];
 export default function Index() {
   const user = useOptionalUser();
   return (
-    <main className="relative min-h-screen bg-white sm:flex sm:items-center sm:justify-center">
-      <div className="relative sm:pb-16 sm:pt-8">
-        <div className="mx-auto max-w-7xl sm:px-6 lg:px-8">
-          <div className="relative shadow-xl sm:overflow-hidden sm:rounded-2xl">
-            <div className="absolute inset-0">
-              <div className="absolute inset-0 bg-indigo-50 mix-blend-multiply" />
-            </div>
-            <div className="relative px-4 pb-8 pt-16 sm:px-6 sm:pb-14 sm:pt-24 lg:px-8 lg:pb-20 lg:pt-32">
-              <h1 className="text-center text-6xl font-extrabold tracking-tight sm:text-8xl lg:text-9xl">
-                <span className="block uppercase text-orange-500 drop-shadow-md">
-                  Kind Retro App
-                </span>
-              </h1>
-              <p className="mx-auto mt-6 max-w-lg text-center text-xl text-black sm:max-w-3xl">
-                Start your retro when you're feeling ready
-              </p>
-              <p>Good feedback is:</p>
-              <ul>
-                <li>Specific - focus on a single thing that can change</li>
-                <li>
-                  Kind - assume that everyone on the team is doing their
-                  personal best
-                </li>
-                <li>
-                  Actionable - ensure that the suggestion gives clear next steps
-                </li>
-              </ul>
-              <div className="mx-auto mt-10 max-w-sm sm:flex sm:max-w-none sm:justify-center">
-                {user ? (
-                  <div className="space-y-4 sm:mx-auto sm:inline-grid sm:grid-cols-2 sm:gap-5 sm:space-y-0">
-                    <Link
-                      to="/boards"
-                      className="flex items-center justify-center rounded-md border border-transparent bg-white px-4 py-3 text-base font-medium text-yellow-700 shadow-sm hover:bg-yellow-50 sm:px-8"
-                    >
-                      View boards for {user.email}
-                    </Link>
-                    <Form action="/logout" method="post">
-                      <button
-                        type="submit"
-                        className="rounded bg-slate-600 px-4 py-2 text-blue-100 hover:bg-blue-500 active:bg-blue-600"
-                      >
-                        Logout
-                      </button>
-                    </Form>
-                  </div>
-                ) : (
-                  <div className="space-y-4 sm:mx-auto sm:inline-grid sm:grid-cols-2 sm:gap-5 sm:space-y-0">
-                    <Link
-                      to="/join"
-                      className="flex items-center justify-center rounded-md border border-transparent bg-white px-4 py-3 text-base font-medium text-yellow-700 shadow-sm hover:bg-yellow-50 sm:px-8"
-                    >
-                      Sign up
-                    </Link>
-                    <Link
-                      to="/login"
-                      className="flex items-center justify-center rounded-md bg-yellow-500 px-4 py-3 font-medium text-white hover:bg-yellow-600"
-                    >
-                      Log In
-                    </Link>
-                    <Link
-                      to="/boards"
-                      className="text-xl text-blue-600 underline"
-                    >
-                      Boards
-                    </Link>
-                  </div>
-                )}
-              </div>
-            </div>
+    <main className="relative min-h-screen flex-col gap-4 bg-gradient-to-b from-orange-400 to-orange-600 sm:flex sm:items-center sm:justify-center">
+      <div className="flex max-w-md flex-col items-center justify-center gap-4">
+        <h1 className="text-4xl font-bold text-white">Kind Retro App</h1>
+        <p className="text-center text-xl font-bold text-white">
+          Good feedback is specific, prescriptive, actionable, referenced, and
+          kind.
+        </p>
+      </div>
+      <div className="flex w-full max-w-md items-center justify-center gap-4 rounded-xl bg-white bg-opacity-10 bg-clip-padding p-8 backdrop-blur-lg backdrop-filter">
+        {user ? (
+          <>
+            <p className="flex items-center justify-center rounded-full  p-2 text-center text-xl font-bold text-white">
+              {user.email.charAt(0).toUpperCase()}
+            </p>
+            <Form action="/logout" method="post">
+              <button
+                type="submit"
+                className="flex items-center justify-center rounded-md bg-white bg-opacity-10 px-4 py-3 font-medium text-white hover:bg-opacity-20"
+              >
+                👋
+              </button>
+            </Form>
+          </>
+        ) : (
+          <div className="space-y-4 sm:mx-auto sm:inline-grid sm:grid-cols-2 sm:gap-5 sm:space-y-0">
+            <Link
+              to="/join"
+              className="flex items-center justify-center rounded-md bg-white bg-opacity-10 px-4 py-3 font-medium text-white hover:bg-opacity-20"
+            >
+              Sign up
+            </Link>
+            <Link
+              to="/login"
+              className="flex items-center justify-center rounded-md bg-indigo-500 px-4 py-3 font-medium text-white hover:bg-indigo-600"
+            >
+              Log In
+            </Link>
           </div>
-        </div>
+        )}
+      </div>
+
+      <div className="w-full max-w-md space-y-8 rounded-xl bg-white bg-opacity-10 bg-clip-padding p-8 ">
+        <Link
+          className="flex items-center justify-center rounded-md bg-white bg-opacity-10 px-4 py-3 font-medium text-white hover:bg-opacity-20"
+          to="/boards"
+        >
+          Start
+        </Link>
       </div>
     </main>
   );
