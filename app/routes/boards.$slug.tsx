f0f27388a7b7c5364bd9deedbe0d7da8f6c93f0a
Jack Howard <jack.howard@expel.io>
2023-05-04 15:01:12 -0700

    feat: Add basic page for handling boards

--- app/routes/boards.$slug.tsx
+++ app/routes/boards.$slug.tsx
@@ -1,6 +1,6 @@
 import type { LoaderArgs } from "@remix-run/node";
 import { json } from "@remix-run/node";
-import { useLoaderData } from "@remix-run/react";
+import { Form, Link, useLoaderData } from "@remix-run/react";
 import { getBoard } from "~/models/board.server";
 import invariant from "tiny-invariant";
 
@@ -22,10 +22,48 @@ export default function BoardSlug() {
   // comes from the params object in loader
   const { board } = useLoaderData<typeof loader>();
   return (
-    <main className="mx-auto max-w-4xl">
-      <h1 className="my-6 border-b-2 text-center text-3xl">
-        Board: {board.title}
-      </h1>
+    <main className="flex h-full w-full flex-col items-center gap-4">
+      <div className="flex w-10/12 flex-col items-start justify-start gap-4">
+        <Link to="/boards" className="text-2xl text-gray-900">
+          ←
+        </Link>
+        {/* todo: implement timer */}
+      </div>
+      <div className="flex w-10/12  items-start justify-between	">
+        <div className="flex flex-col">
+          <h1 className="text-3xl">Retrospective</h1>
+          <h2 className="text-2xl text-gray-600">{board.title}</h2>
+        </div>
+        <div className="flex flex-row gap-2">
+          <Form>
+            <label htmlFor="invite" className="text-gray-600">
+              Invite User By Email
+            </label>
+            <input
+              id="invite"
+              name="invite"
+              type="email"
+              className="block w-full rounded-md border-gray-300 shadow-sm focus:border-orange-500 focus:ring-orange-500"
+              placeholder="Email"
+            />
+            <button
+              type="submit"
+              className="flex items-center justify-center rounded-md bg-orange-500 px-4 py-3 font-medium text-white hover:bg-orange-600"
+            >
+              Invite
+            </button>
+          </Form>
+        </div>
+      </div>
+      <div className="flex w-10/12 flex-col items-center gap-2">
+        <h3 className="text-center text-xl text-gray-600">People</h3>
+        <ul className="flex flex-row flex-row gap-2 bg-clip-padding">
+          {["J", "K", "L"].map((letter) => (
+            <li key={letter}>{letter}</li>
+            // todo: add pronouns
+          ))}
+        </ul>
+      </div>
     </main>
   );
 }
--- app/routes/boards._index.tsx
+++ app/routes/boards._index.tsx
@@ -14,23 +14,35 @@ export default function Boards() {
   const { boards } = useLoaderData<typeof loader>();
 
   return (
-    <main>
-      <h1>Retros</h1>
-      <ul>
-        {boards.map((board) => (
-          <li key={board.slug}>
-            <Link to={board.slug} className="text-blue-600 underline">
+    <main className="flex min-h-screen flex-col items-center justify-center gap-4 bg-gradient-to-b from-orange-400 to-orange-600">
+      <Link to="/" className="text-2xl text-white">
+        ←
+      </Link>
+      <h1 className="text-center text-4xl font-bold text-white">Boards</h1>
+      <ul className="flex flex-col gap-4 rounded-xl bg-white bg-opacity-10 bg-clip-padding p-8 backdrop-blur-lg backdrop-filter">
+        {boards.map((board: any) => (
+          <li
+            className="text-center text-xl font-bold text-white"
+            key={board.slug}
+          >
+            <Link
+              to={board.slug}
+              className="text-center text-xl font-bold text-white underline"
+            >
               {board.title}
             </Link>
           </li>
         ))}
       </ul>
-      {/* you get relative links here based on the parent route */}
-      {/* Did you notice that the to prop is just "admin" and it linked to
+
+      <div className="flex items-center justify-center rounded-md bg-white bg-opacity-10 px-4 py-3 font-medium text-white hover:bg-opacity-20">
+        {/* you get relative links here based on the parent route */}
+        {/* Did you notice that the to prop is just "admin" and it linked to
       /posts/admin? With Remix, you get relative links. */}
-      <Link to="admin" className="text-red-600 underline">
-        Admin
-      </Link>
+        <Link to="admin" className="text-xl text-white underline">
+          Admin
+        </Link>
+      </div>
     </main>
   );
 }
